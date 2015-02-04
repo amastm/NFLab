e8d0bac8a1460f69aff9a850f5ec358be3d23301
Guillaume Pernelle <gpernelle@gmail.com>
2015-02-04 21:26:43 +0000

    gp changes

--- GA_snippets.py
+++ GA_snippets.py
@@ -1,7 +1,3 @@
-Guillaume=False #for file paths
-
-print "example usage in slicer python console: geneticAlgorithm(24)"
-
 import random, copy
 import operator
 import NeedleFinder
@@ -12,40 +8,15 @@ l = NeedleFinder.NeedleFinderLogic()
 import time as t
 
 path = [ 0 for i in range(100)]
-
-#Andres file system (cases copies from AMIGO share)
-path[24] = '/home/amastmeyer/Pictures/Case  024/NRRD/Manual/2013-02-25-Scene-without-CtrPt.mrml'
-path[29] = '/home/amastmeyer/Pictures/Case  029/NRRD/Manual/2013-02-26-Scene-without-CtrPts.mrml'
-path[30] = '/home/amastmeyer/Pictures/Case  030/NRRD/Manual/2013-02-26-Scene-without-CtrPt.mrml'
-path[31] = '/home/amastmeyer/Pictures/Case  031/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-path[34] = '/home/amastmeyer/Pictures/Case  034/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-path[35] = '/home/amastmeyer/Pictures/Case  035/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-path[37] = '/home/amastmeyer/Pictures/Case  037/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-path[38] = '/home/amastmeyer/Pictures/Case  038/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-path[40] = '/home/amastmeyer/Pictures/Case  040/NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-
-#MICCAI13 results in Andres files
-path[24] = '/home/amastmeyer/Pictures/Case  024/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[28] = '/home/amastmeyer/Pictures/Case  028/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[29] = '/home/amastmeyer/Pictures/Case  029/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[30] = '/home/amastmeyer/Pictures/Case  030/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[31] = '/home/amastmeyer/Pictures/Case  031/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[33] = '/home/amastmeyer/Pictures/Case  033/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[34] = '/home/amastmeyer/Pictures/Case  034/NRRD/Auto-Eval-LB/SceneNoCtrlPts.mrml'
-path[37] = '/home/amastmeyer/Pictures/Case  037/NRRD/Manual Alireza/SceneNoCtrlPts.mrml'
-path[38] = '/home/amastmeyer/Pictures/Case  038/NRRD/Manual Alireza/SceneNoCtrlPts.mrml'
-path[40] = '/home/amastmeyer/Pictures/Case  040/NRRD/Manual Alireza/SceneNoCtrlPts.mrml'
-
-if Guillaume: #Guillaumes file system
-  path[24] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 24 NRRD/Manual/2013-02-25-Scene-without-CtrPt.mrml'
-  path[29] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 29 NRRD/Manual/2013-02-26-Scene-without-CtrPts.mrml'
-  path[30] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 30 NRRD/Manual/2013-02-26-Scene-without-CtrPt.mrml'
-  path[31] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 31 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-  path[34] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 34 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-  path[35] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 35 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-  path[37] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 37 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-  path[38] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 38 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
-  path[40] = '/Users/guillaume/Dropbox/AMIGO Gyn Data NRRD (1)/Case 40 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[24] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 24 NRRD/Manual/2013-02-25-Scene-without-CtrPt.mrml'
+path[29] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 29 NRRD/Manual/2013-02-26-Scene-without-CtrPts.mrml'
+path[30] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 30 NRRD/Manual/2013-02-26-Scene-without-CtrPt.mrml'
+path[31] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 31 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[34] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 34 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[35] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 35 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[37] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 37 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[38] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 38 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
+path[40] = '/Users/GP1514/Dropbox/AMIGO Gyn Data NRRD (1)/Case 40 NRRD/Manual/2013-02-27-Scene-without-CtrPts.mrml'
 
 def resetNeedleDetection(l, script=False):
   while slicer.util.getNodes('python-catch*') != {}:
@@ -70,10 +41,7 @@ def costFunction(chrm,caseID, writeResults = True):
   l.startValidation(script=True)
   results = l.evaluate(script=True) # calculate HD distances
   if writeResults:
-    if Guillaume:
-      l.exportEvaluation(results, '/Users/guillaume/Projects/github/NeedleFinderProjectWeek/'+str(caseID)+'.csv')
-    else:
-      l.exportEvaluation(results, '/home/amastmeyer/'+str(caseID)+'-cost.csv')
+    l.exportEvaluation(results, '/Users/GP1514/Projects/github/NeedleFinderProjectWeek/'+str(caseID)+'.csv')
   HD=np.array(results)
   # print HD
   cost = np.sum(HD[:,0]>2)
@@ -109,7 +77,7 @@ rangeTable.append([0,25])
 rangeTable.append([0,10])
 
 
-def geneticAlgorithm(caseID, populationSize=10, nbOfGenerations=100):
+def geneticAlgorithm(caseID, populationSize, nbOfGenerations):
   firstTime = 1
   slicer.util.loadScene( path[caseID] )
   # [ctrlpt, radius, sigma, neigh, center]
@@ -133,22 +101,22 @@ def geneticAlgorithm(caseID, populationSize=10, nbOfGenerations=100):
     message += '\n Do you want to continue?'
     ret = messageBox = qt.QMessageBox.question( qt.QDialog(), 'Attention',message,qt.QMessageBox.Ok, qt.QMessageBox.Cancel)
   for generation in range(nbOfGenerations):
+    # print 'Generation ', generation
+    # print sample_pop, '\n'
     if ret != qt.QMessageBox.Ok:
       break
     # print "generation: ",generation
     #high cost ~ low fitness 1/... 
     fitness_list=[]  
-    for i in range(popSize):
+    for i in range(len(sample_pop)):
+      # print '\t',i,'/',len(sample_pop)
       if not sample_pop[i] in tried_params: #to avoid doing several times the same calculation
         params = sample_pop[i]
         fitness = costFunction(params, caseID)[1]
         tried_params.append(params)
         tried_fitness.append(fitness)
         fitness_list.append(fitness)
-        if Guillaume:
-          l.exportEvaluation(params+[fitness], '/Users/guillaume/Projects/github/NeedleFinderProjectWeek/'+str(caseID)+'-fitness.csv')
-        else:
-          l.exportEvaluation(params+[fitness], '/home/amastmeyer/'+str(caseID)+'-fitness.csv')
+        l.exportEvaluation(params+[fitness], '/Users/GP1514/Projects/github/NeedleFinderProjectWeek/'+str(caseID)+'-fitness.csv')
         # print fitness_list
         fitness_sum = reduce( operator.add, fitness_list)
         prob_list =map((lambda x: x/fitness_sum),fitness_list)
@@ -158,33 +126,43 @@ def geneticAlgorithm(caseID, populationSize=10, nbOfGenerations=100):
           cum_prob_list.append( cum_value + prob )
           cum_value += prob
         cum_prob_list[-1] = 1.0
+      else:
+        print '\t\tskipped'
     #selection
-    selected = []
-    size = popSize #*0.75
+    selected = [np.random.randint(0,len(sample_pop))]
+    size = len(sample_pop) #*0.75
     for i in xrange(size):
       rn = random.random()
       for j, cum_prob in enumerate(cum_prob_list):
-        if rn<= cum_prob:
+        if rn<= cum_prob and j not in selected:
           selected.append(j)
           break
     # simple crossover (1 point)
-    parent1=sample_pop[selected[np.random.randint(0,len(selected))]]
-    parent2=sample_pop[selected[np.random.randint(0,len(selected))]]
+    parent1=sample_pop[selected[0]]
+    parent2=sample_pop[selected[1]]
+    # print selected
+    # print 'parent'
+    # print parent1
+    # print parent2
     # for i in range(5):
     #   offspring[i]=[]
     # crossover point
     pt = np.random.randint(0,5)
     offspring1 = parent1[:pt] + parent2[pt:]
     offspring2 = parent2[:pt] + parent1[pt:]
-    sample_pop.append(offspring1)
-    sample_pop.append(offspring2)
+    # print 'first offsprings:'
+    # print offspring1, offspring2
+    if not offspring1 in sample_pop: sample_pop.append(offspring1)
+    if not offspring2 in sample_pop: sample_pop.append(offspring2)
     # multi crossover points 
     pt1 = np.random.randint(0,5)
     pt2 = np.random.randint(pt1,5)
     offspring1 = parent1[:pt1] + parent2[pt1:pt2] + parent1[pt2:]
     offspring2 = parent2[:pt1] + parent1[pt1:pt2] + parent2[pt2:]
-    sample_pop.append(offspring1)
-    sample_pop.append(offspring2)
+    # print 'second offsprings'
+    # print offspring1, offspring2
+    if not offspring1 in sample_pop: sample_pop.append(offspring1)
+    if not offspring2 in sample_pop: sample_pop.append(offspring2)
     # value mutation
     chrm = sample_pop[selected[np.random.randint(0, len(selected))]]
     element_position = random.randint(0, len(chrm)-1 )
